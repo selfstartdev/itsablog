4b3f2863cbe7d6fa9f4d82e1a6bc62179baac577
Brian Brennan <me@brianbrennan.com>
2017-11-22 15:22:06 -0500

    More Unit Tests

--- test/ItsABlog.spec.js
+++ test/ItsABlog.spec.js
@@ -15,6 +15,48 @@ describe('ItsABlog', () => {
         itsABlog = new ItsABlog();
     });
 
+    describe('constructor', () => {
+        let defaultOptions, customOptions, currentOptions;
+
+        beforeEach(() => {
+            defaultOptions = {
+                metaTagStart: '<meta>',
+                metaTagEnd: '</meta>',
+                dir: 'blog',
+                encoding: 'utf-8',
+                pretty: true,
+                output: 'blog.json'
+            };
+        });
+
+        it('should set the default options', () => {
+            itsABlog = new ItsABlog();
+            expect(itsABlog.options).to.eql(defaultOptions);
+        });
+
+        it('should be able to override any option given', () => {
+             customOptions = {
+                 metaTagStart: 'test',
+                 metaTagEnd: 'test',
+                 dir: 'test',
+                 encoding: 'test',
+                 pretty: false,
+                 output: 'test'
+             };
+
+            currentOptions = {};
+
+            Object.keys(customOptions).forEach((key) => {
+                currentOptions[key] = customOptions[key];
+                itsABlog = new ItsABlog(currentOptions);
+
+                Object.keys(currentOptions).forEach((key) => {
+                    expect(itsABlog.options[key]).to.eql(currentOptions[key]);
+                });
+            });
+        });
+    });
+
     describe('Runner Methods', () => {
         describe('#getPosts', () => {
             let configureFileManifestStub, fakeFileManifest;
@@ -72,36 +114,57 @@ describe('ItsABlog', () => {
 
     describe('Builder Methods', () => {
         describe('#configureFileManifest', () => {
-            let stubList = [],
-                methodNames = ['getNamesOfFilesFromDir', 'initiateFileManifest', 'initializeMetaData',
-                    'configureCustomMetaData', 'removeMetaDataString', 'compileContent',
-                    'prettifyFileManifest'];
+            let getNamesOfFilesFromDirStub, initiateFileManifestStub, initializeMetaDataStub,
+                configureCustomMetaDataStub, removeMetaDataStringStub, compileContentStub,
+                prettifyFileManifestStub;
 
             beforeEach(() => {
-                methodNames.forEach((methodName) => {
-                    stubList.push(sinon.stub(itsABlog, methodName));
-                });
+                getNamesOfFilesFromDirStub = sinon.stub(itsABlog, 'getNamesOfFilesFromDir');
+                initiateFileManifestStub = sinon.stub(itsABlog, 'initiateFileManifest');
+                initializeMetaDataStub = sinon.stub(itsABlog, 'initializeMetaData');
+                configureCustomMetaDataStub = sinon.stub(itsABlog, 'configureCustomMetaData');
+                removeMetaDataStringStub = sinon.stub(itsABlog, 'removeMetaDataString');
+                compileContentStub = sinon.stub(itsABlog, 'compileContent');
+                prettifyFileManifestStub = sinon.stub(itsABlog, 'prettifyFileManifest');
             });
 
             afterEach(() => {
-                methodNames.forEach((methodName) => {
-                    itsABlog[methodName].restore();
-                });
+                itsABlog.getNamesOfFilesFromDir.restore();
+                itsABlog.initiateFileManifest.restore();
+                itsABlog.initializeMetaData.restore();
+                itsABlog.configureCustomMetaData.restore();
+                itsABlog.removeMetaDataString.restore();
+                itsABlog.compileContent.restore();
+                itsABlog.prettifyFileManifest.restore();
             });
 
             it('should not do anything if the fileManifest is defined', () => {
                 itsABlog.fileManifest = {};
                 itsABlog.configureFileManifest();
-                stubList.forEach((stub) => {
-                    expect(stub).to.have.not.been.calledWith();
-                });
+                expect(getNamesOfFilesFromDirStub).to.have.not.been.called;
+                expect(initiateFileManifestStub).to.have.not.been.called;
+                expect(initializeMetaDataStub).to.have.not.been.called;
+                expect(configureCustomMetaDataStub).to.have.not.been.called;
+                expect(removeMetaDataStringStub).to.have.not.been.called;
+                expect(compileContentStub).to.have.not.been.called;
+                expect(prettifyFileManifestStub).to.have.not.been.called;
             });
 
             it('should go through all the proper calls', () => {
                 itsABlog.configureFileManifest();
-                stubList.forEach((stub) => {
-                    expect(stub).to.have.been.called;
-                });
+                expect(getNamesOfFilesFromDirStub).to.have.been.called;
+                expect(initiateFileManifestStub).to.have.been.called;
+                expect(initializeMetaDataStub).to.have.been.called;
+                expect(configureCustomMetaDataStub).to.have.been.called;
+                expect(removeMetaDataStringStub).to.have.been.called;
+                expect(compileContentStub).to.have.been.called;
+                expect(prettifyFileManifestStub).to.have.been.called;
+            });
+
+            it('should call to prettify if the option is set to false', () => {
+                itsABlog.options.pretty = false;
+                itsABlog.configureFileManifest();
+                expect(prettifyFileManifestStub).to.have.not.been.called;
             });
         });
     });
